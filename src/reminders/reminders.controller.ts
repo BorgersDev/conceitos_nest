961cf4376b0a821f6e86598bc8170e0ede7b18f5
Arthur <borgesarthur.dev@gmail.com>
2025-09-26 15:41:16 -0300

    cria metodos pro CRUD

--- src/reminders/reminders.controller.ts
+++ src/reminders/reminders.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('reminders')
 export class RemindersController {
@@ -8,7 +8,25 @@ export class RemindersController {
   }
 
   @Get(':id')
-  findOne() {
-    return 'Return one reminder'
+  findOne(@Param('id') id: string) {
+    return `Return one reminder with id ${id}`
+  }
+
+  @Post()
+  create(@Body() body: any) {
+    return `Create a new reminder with title "${body.title}"`
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body: any) {
+    return {
+      id,
+      ...body,
+    }
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `Delete reminder with id: ${id}`
   }
 }
