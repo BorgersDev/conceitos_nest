41d92723198fd1cc0ae151b72443ceb4bbcd1bda
Arthur Borges <154439946+BorgersDev@users.noreply.github.com>
2025-09-29 00:18:00 -0300

    feat: adiciona docker e typeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RemindersModule } from './reminders/reminders.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [RemindersModule],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'postgres',
+    database: 'conceitos-nest',
+    autoLoadEntities: true,
+    synchronize: true,
+  }),RemindersModule],
   controllers: [AppController],
   providers: [AppService],
 })
