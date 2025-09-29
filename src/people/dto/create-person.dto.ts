98cc33e5ec85dc565202e093b4ce6f1cce475649
Arthur Borges <154439946+BorgersDev@users.noreply.github.com>
2025-09-29 07:33:23 -0300

    feat: cria entidade e dto para pessoa e faz logica da função create

--- src/people/dto/create-person.dto.ts
+++ src/people/dto/create-person.dto.ts
@@ -1,1 +1,18 @@
-export class CreatePersonDto {}
+import { IsEmail, IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
+
+export class CreatePersonDto {
+
+  @IsEmail()
+  email: string;
+  
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(5)
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(100)
+  name: string;
+}
--- src/people/people.service.ts
+++ src/people/people.service.ts
@@ -1,11 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreatePersonDto } from './dto/create-person.dto';
 import { UpdatePersonDto } from './dto/update-person.dto';
+import { Repository } from 'typeorm';
+import { Person } from './entities/person.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class PeopleService {
-  create(createPersonDto: CreatePersonDto) {
-    return 'This action adds a new person';
+  constructor(
+    @InjectRepository(Person)
+    private readonly peopleRepository: Repository<Person>
+  ) {}
+  async create(createPersonDto: CreatePersonDto) {
+    try {
+      const personData = {
+        name: createPersonDto.name,
+        passwordHash: createPersonDto.password,
+        email: createPersonDto.email,
+      }
+      const newPerson = this.peopleRepository.create(personData);
+      return await this.peopleRepository.save(newPerson);
+    } catch (error) {
+      if(error.code === '23505') {
+        throw new ConflictException('Email already exists');
+      }
+      throw error;
+    }
   }
 
   findAll() {
