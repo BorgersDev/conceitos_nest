ac600b76879ea849e3346a3de3d698cb9b75f093
Arthur Borges <154439946+BorgersDev@users.noreply.github.com>
2025-10-22 05:40:03 -0300

    feat: importa dependências corretas nos módulos

--- src/reminders/reminders.module.ts
+++ src/reminders/reminders.module.ts
@@ -3,9 +3,10 @@ import { RemindersController } from './reminders.controller';
 import { RemindersService } from './reminders.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Reminder } from './entities/reminder.entity';
+import { PeopleModule } from 'src/people/people.module';
 
 @Module({
-  imports: [ TypeOrmModule.forFeature([Reminder]), ],
+  imports: [ TypeOrmModule.forFeature([Reminder]), PeopleModule ],
   controllers: [RemindersController],
   providers: [RemindersService]
 })
